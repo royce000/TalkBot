8e95d9422ae847071ea966f35b51a2786ab3ce32
kitaKist <kitano@s14.yeskist.net>
2015-06-08 15:47:53 +0900

    言葉のデータクラスを追加

---
+++ TalkBot/TalkData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TalkBot
+{
+    public class TalkData
+    {
+        private DataTable talkDataTable = new DataTable();   // 会話で使用する言葉データを保持するテーブル
+
+
+        public TalkData()
+        {
+            // テーブルの初期化
+        }
+
+        /// <summary>
+        /// 会話で使用する言葉のデータテーブルを取得
+        /// </summary>
+        public DataTable TalkDataTable { get { return talkDataTable; } }
+    }
+}

28e37c10611a16921b314c7d9f9169f5ba27dd09
kitaKist <kitano@s14.yeskist.net>
2015-06-22 10:34:12 +0900

    デバッグのログ追加メソッドを追加

--- TalkBot/Form1.cs
+++ TalkBot/Form1.cs
@@ -17,5 +17,22 @@ namespace TalkBot
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// デバッグログにログを追加するメソッド
+        /// </summary>
+        /// <param name="name">名前</param>
+        /// <param name="text">ログ内容</param>
+        public void AddDebugLog(string name, string text)
+        {
+            DateTime now = DateTime.Now;
+
+            string time = now.ToString("HH:mm:ss");
+            string log = name == "" ? string.Format("[{0}] <?> {1}", time, text) : string.Format("[{0}] <1> {2}", time, name, text);
+
+            debugLogTextBox.AppendText(log + "\n");
+            debugLogTextBox.SelectionStart = debugLogTextBox.Text.Length;
+            debugLogTextBox.ScrollToCaret();
+        }
     }
 }

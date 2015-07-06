84428e9c5333549b49aa2320f12229e000666d74
kitaKist <kitano@s14.yeskist.net>
2015-07-06 10:28:59 +0900

    顔の画像のプレビュー機能を実装

--- TalkBot/Form1.Designer.cs
+++ TalkBot/Form1.Designer.cs
@@ -49,7 +49,7 @@
             this.EOutputTextBox = new System.Windows.Forms.TextBox();
             this.EInputTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.FacePreviewButton = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.facePicBox)).BeginInit();
@@ -226,7 +226,7 @@
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.FacePreviewButton);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.EFaceComboBox);
             this.groupBox1.Controls.Add(this.educationButton);
@@ -260,6 +260,7 @@
             this.EFaceComboBox.Name = "EFaceComboBox";
             this.EFaceComboBox.Size = new System.Drawing.Size(276, 20);
             this.EFaceComboBox.TabIndex = 5;
+            this.EFaceComboBox.SelectedIndexChanged += new System.EventHandler(this.EFaceComboBox_SelectedIndexChanged);
             this.EFaceComboBox.Format += new System.Windows.Forms.ListControlConvertEventHandler(this.EFaceComboBox_Format);
             // 
             // educationButton
@@ -310,15 +311,16 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "反応する文字列";
             // 
-            // button1
+            // FacePreviewButton
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(378, 68);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(80, 23);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "顔のプレビュー";
-            this.button1.UseVisualStyleBackColor = true;
+            this.FacePreviewButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.FacePreviewButton.Location = new System.Drawing.Point(378, 68);
+            this.FacePreviewButton.Name = "FacePreviewButton";
+            this.FacePreviewButton.Size = new System.Drawing.Size(80, 23);
+            this.FacePreviewButton.TabIndex = 7;
+            this.FacePreviewButton.Text = "顔のプレビュー";
+            this.FacePreviewButton.UseVisualStyleBackColor = true;
+            this.FacePreviewButton.Click += new System.EventHandler(this.FacePreviewButton_Click);
             // 
             // Form1
             // 
@@ -368,7 +370,7 @@
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.DataGridView TalkDataView;
         private System.Windows.Forms.TextBox BotText;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button FacePreviewButton;
     }
 }
 
